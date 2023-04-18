3ed28b93936d889e9bbec5e6103ee7d1d24739cf
Administrator <washingtton_hotmail.com>
2023-04-17 22:31:29 -0300

    2

--- SuporteHelp/Descricao_Coluna.Designer.vb
+++ SuporteHelp/Descricao_Coluna.Designer.vb
@@ -22,17 +22,36 @@ Partial Class Descricao_Coluna
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Descricao_Coluna))
+        SairTabelaColuna = New Button()
         SuspendLayout()
+        ' 
+        ' SairTabelaColuna
+        ' 
+        SairTabelaColuna.Font = New Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        SairTabelaColuna.Image = CType(resources.GetObject("SairTabelaColuna.Image"), Image)
+        SairTabelaColuna.ImageAlign = ContentAlignment.MiddleRight
+        SairTabelaColuna.Location = New Point(735, 699)
+        SairTabelaColuna.Margin = New Padding(3, 4, 3, 4)
+        SairTabelaColuna.Name = "SairTabelaColuna"
+        SairTabelaColuna.Size = New Size(135, 51)
+        SairTabelaColuna.TabIndex = 22
+        SairTabelaColuna.Text = "SAIR"
+        SairTabelaColuna.TextAlign = ContentAlignment.MiddleLeft
+        SairTabelaColuna.UseVisualStyleBackColor = True
         ' 
         ' Descricao_Coluna
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(882, 753)
+        Controls.Add(SairTabelaColuna)
         FormBorderStyle = FormBorderStyle.FixedSingle
         MaximizeBox = False
         Name = "Descricao_Coluna"
         Text = "Descricao_Coluna"
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents SairTabelaColuna As Button
 End Class
