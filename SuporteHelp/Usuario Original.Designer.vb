7918902877763ff5d775cc8c499a05de8100b82a
Administrator <washingtton_hotmail.com>
2023-05-08 17:31:28 -0300

    1

--- SuporteHelp/Usuario Original.Designer.vb
+++ SuporteHelp/Usuario Original.Designer.vb
@@ -23,91 +23,97 @@ Partial Class Usuario_Original
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Usuario_Original))
-        SelecionarBancoLbl = New Label()
-        SelecionarBancoBbx = New ComboBox()
-        ConectarBtn = New Button()
-        SenhaTabelaTxb = New TextBox()
-        NomeConectarTabelaTxb = New TextBox()
+        SelecionarBancoUsuarioLbl = New Label()
+        SelecionarBancoUsuarioBbx = New ComboBox()
+        ConectarUsuarioBtn = New Button()
+        SenhaUsusarioTxb = New TextBox()
+        NomeusuarioTxb = New TextBox()
         ServidorUsuarioTxb = New TextBox()
-        SenhaConectarLbl = New Label()
+        SenhaUsuarioLbl = New Label()
         UsuarioLbl = New Label()
         ServidorUsuarioLbl = New Label()
+        ExecutarTodosUsuariosBtn = New Button()
+        ExecutarTodosLbl = New Label()
+        Button1 = New Button()
+        SelecionarUsuarioCbx = New ComboBox()
+        SelecionarUsuarioLbl = New Label()
+        ExecutarUsuarioEspecificoBtn = New Button()
         SuspendLayout()
         ' 
-        ' SelecionarBancoLbl
+        ' SelecionarBancoUsuarioLbl
         ' 
-        SelecionarBancoLbl.AutoSize = True
-        SelecionarBancoLbl.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
-        SelecionarBancoLbl.Location = New Point(563, 12)
-        SelecionarBancoLbl.Name = "SelecionarBancoLbl"
-        SelecionarBancoLbl.Size = New Size(127, 20)
-        SelecionarBancoLbl.TabIndex = 48
-        SelecionarBancoLbl.Text = "Selecionar Banco"
+        SelecionarBancoUsuarioLbl.AutoSize = True
+        SelecionarBancoUsuarioLbl.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        SelecionarBancoUsuarioLbl.Location = New Point(580, 14)
+        SelecionarBancoUsuarioLbl.Name = "SelecionarBancoUsuarioLbl"
+        SelecionarBancoUsuarioLbl.Size = New Size(127, 20)
+        SelecionarBancoUsuarioLbl.TabIndex = 48
+        SelecionarBancoUsuarioLbl.Text = "Selecionar Banco"
         ' 
-        ' SelecionarBancoBbx
+        ' SelecionarBancoUsuarioBbx
         ' 
-        SelecionarBancoBbx.FormattingEnabled = True
-        SelecionarBancoBbx.Location = New Point(563, 36)
-        SelecionarBancoBbx.Margin = New Padding(3, 4, 3, 4)
-        SelecionarBancoBbx.Name = "SelecionarBancoBbx"
-        SelecionarBancoBbx.Size = New Size(225, 28)
-        SelecionarBancoBbx.TabIndex = 47
+        SelecionarBancoUsuarioBbx.FormattingEnabled = True
+        SelecionarBancoUsuarioBbx.Location = New Point(580, 38)
+        SelecionarBancoUsuarioBbx.Margin = New Padding(3, 4, 3, 4)
+        SelecionarBancoUsuarioBbx.Name = "SelecionarBancoUsuarioBbx"
+        SelecionarBancoUsuarioBbx.Size = New Size(225, 28)
+        SelecionarBancoUsuarioBbx.TabIndex = 47
         ' 
-        ' ConectarBtn
+        ' ConectarUsuarioBtn
         ' 
-        ConectarBtn.Font = New Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
-        ConectarBtn.Image = CType(resources.GetObject("ConectarBtn.Image"), Image)
-        ConectarBtn.ImageAlign = ContentAlignment.MiddleLeft
-        ConectarBtn.Location = New Point(428, 27)
-        ConectarBtn.Margin = New Padding(3, 4, 3, 4)
-        ConectarBtn.Name = "ConectarBtn"
-        ConectarBtn.Size = New Size(111, 37)
-        ConectarBtn.TabIndex = 46
-        ConectarBtn.Text = "Conectar"
-        ConectarBtn.TextAlign = ContentAlignment.MiddleRight
-        ConectarBtn.UseVisualStyleBackColor = True
+        ConectarUsuarioBtn.Font = New Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        ConectarUsuarioBtn.Image = CType(resources.GetObject("ConectarUsuarioBtn.Image"), Image)
+        ConectarUsuarioBtn.ImageAlign = ContentAlignment.MiddleLeft
+        ConectarUsuarioBtn.Location = New Point(445, 29)
+        ConectarUsuarioBtn.Margin = New Padding(3, 4, 3, 4)
+        ConectarUsuarioBtn.Name = "ConectarUsuarioBtn"
+        ConectarUsuarioBtn.Size = New Size(111, 37)
+        ConectarUsuarioBtn.TabIndex = 46
+        ConectarUsuarioBtn.Text = "Conectar"
+        ConectarUsuarioBtn.TextAlign = ContentAlignment.MiddleRight
+        ConectarUsuarioBtn.UseVisualStyleBackColor = True
         ' 
-        ' SenhaTabelaTxb
+        ' SenhaUsusarioTxb
         ' 
-        SenhaTabelaTxb.Location = New Point(299, 33)
-        SenhaTabelaTxb.Margin = New Padding(3, 4, 3, 4)
-        SenhaTabelaTxb.Name = "SenhaTabelaTxb"
-        SenhaTabelaTxb.Size = New Size(111, 27)
-        SenhaTabelaTxb.TabIndex = 45
+        SenhaUsusarioTxb.Location = New Point(316, 35)
+        SenhaUsusarioTxb.Margin = New Padding(3, 4, 3, 4)
+        SenhaUsusarioTxb.Name = "SenhaUsusarioTxb"
+        SenhaUsusarioTxb.Size = New Size(111, 27)
+        SenhaUsusarioTxb.TabIndex = 45
         ' 
-        ' NomeConectarTabelaTxb
+        ' NomeusuarioTxb
         ' 
-        NomeConectarTabelaTxb.Location = New Point(160, 33)
-        NomeConectarTabelaTxb.Margin = New Padding(3, 4, 3, 4)
-        NomeConectarTabelaTxb.Name = "NomeConectarTabelaTxb"
-        NomeConectarTabelaTxb.Size = New Size(133, 27)
-        NomeConectarTabelaTxb.TabIndex = 44
+        NomeusuarioTxb.Location = New Point(177, 35)
+        NomeusuarioTxb.Margin = New Padding(3, 4, 3, 4)
+        NomeusuarioTxb.Name = "NomeusuarioTxb"
+        NomeusuarioTxb.Size = New Size(133, 27)
+        NomeusuarioTxb.TabIndex = 44
         ' 
         ' ServidorUsuarioTxb
         ' 
-        ServidorUsuarioTxb.Location = New Point(3, 33)
+        ServidorUsuarioTxb.Location = New Point(20, 35)
         ServidorUsuarioTxb.Margin = New Padding(3, 4, 3, 4)
         ServidorUsuarioTxb.Name = "ServidorUsuarioTxb"
         ServidorUsuarioTxb.Size = New Size(142, 27)
         ServidorUsuarioTxb.TabIndex = 43
         ' 
-        ' SenhaConectarLbl
+        ' SenhaUsuarioLbl
         ' 
-        SenhaConectarLbl.AutoSize = True
-        SenhaConectarLbl.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
-        SenhaConectarLbl.ForeColor = Color.Blue
-        SenhaConectarLbl.Location = New Point(299, 9)
-        SenhaConectarLbl.Name = "SenhaConectarLbl"
-        SenhaConectarLbl.Size = New Size(51, 20)
-        SenhaConectarLbl.TabIndex = 42
-        SenhaConectarLbl.Text = "Senha"
+        SenhaUsuarioLbl.AutoSize = True
+        SenhaUsuarioLbl.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        SenhaUsuarioLbl.ForeColor = Color.Blue
+        SenhaUsuarioLbl.Location = New Point(316, 11)
+        SenhaUsuarioLbl.Name = "SenhaUsuarioLbl"
+        SenhaUsuarioLbl.Size = New Size(51, 20)
+        SenhaUsuarioLbl.TabIndex = 42
+        SenhaUsuarioLbl.Text = "Senha"
         ' 
         ' UsuarioLbl
         ' 
         UsuarioLbl.AutoSize = True
         UsuarioLbl.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
         UsuarioLbl.ForeColor = Color.Blue
-        UsuarioLbl.Location = New Point(160, 6)
+        UsuarioLbl.Location = New Point(177, 8)
         UsuarioLbl.Name = "UsuarioLbl"
         UsuarioLbl.Size = New Size(63, 20)
         UsuarioLbl.TabIndex = 41
@@ -118,24 +124,92 @@ Partial Class Usuario_Original
         ServidorUsuarioLbl.AutoSize = True
         ServidorUsuarioLbl.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
         ServidorUsuarioLbl.ForeColor = Color.Red
-        ServidorUsuarioLbl.Location = New Point(3, 6)
+        ServidorUsuarioLbl.Location = New Point(20, 8)
         ServidorUsuarioLbl.Name = "ServidorUsuarioLbl"
         ServidorUsuarioLbl.Size = New Size(142, 20)
         ServidorUsuarioLbl.TabIndex = 40
         ServidorUsuarioLbl.Text = "Informe o Servidor"
         ' 
+        ' ExecutarTodosUsuariosBtn
+        ' 
+        ExecutarTodosUsuariosBtn.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        ExecutarTodosUsuariosBtn.Location = New Point(92, 154)
+        ExecutarTodosUsuariosBtn.Name = "ExecutarTodosUsuariosBtn"
+        ExecutarTodosUsuariosBtn.Size = New Size(111, 37)
+        ExecutarTodosUsuariosBtn.TabIndex = 49
+        ExecutarTodosUsuariosBtn.Text = "Executar"
+        ExecutarTodosUsuariosBtn.UseVisualStyleBackColor = True
+        ' 
+        ' ExecutarTodosLbl
+        ' 
+        ExecutarTodosLbl.AutoSize = True
+        ExecutarTodosLbl.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        ExecutarTodosLbl.Location = New Point(23, 117)
+        ExecutarTodosLbl.Name = "ExecutarTodosLbl"
+        ExecutarTodosLbl.Size = New Size(281, 23)
+        ExecutarTodosLbl.TabIndex = 50
+        ExecutarTodosLbl.Text = "Corrigir Todos os Usuarios Orfãos "
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.Image = CType(resources.GetObject("Button1.Image"), Image)
+        Button1.ImageAlign = ContentAlignment.MiddleRight
+        Button1.Location = New Point(703, 260)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(117, 43)
+        Button1.TabIndex = 51
+        Button1.Text = "SAIR"
+        Button1.TextAlign = ContentAlignment.MiddleLeft
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' SelecionarUsuarioCbx
+        ' 
+        SelecionarUsuarioCbx.FormattingEnabled = True
+        SelecionarUsuarioCbx.Location = New Point(373, 154)
+        SelecionarUsuarioCbx.Margin = New Padding(3, 4, 3, 4)
+        SelecionarUsuarioCbx.Name = "SelecionarUsuarioCbx"
+        SelecionarUsuarioCbx.Size = New Size(225, 28)
+        SelecionarUsuarioCbx.TabIndex = 52
+        ' 
+        ' SelecionarUsuarioLbl
+        ' 
+        SelecionarUsuarioLbl.AutoSize = True
+        SelecionarUsuarioLbl.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        SelecionarUsuarioLbl.Location = New Point(414, 117)
+        SelecionarUsuarioLbl.Name = "SelecionarUsuarioLbl"
+        SelecionarUsuarioLbl.Size = New Size(157, 23)
+        SelecionarUsuarioLbl.TabIndex = 53
+        SelecionarUsuarioLbl.Text = "Selecionar Usuario"
+        ' 
+        ' ExecutarUsuarioEspecificoBtn
+        ' 
+        ExecutarUsuarioEspecificoBtn.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point)
+        ExecutarUsuarioEspecificoBtn.Location = New Point(638, 148)
+        ExecutarUsuarioEspecificoBtn.Name = "ExecutarUsuarioEspecificoBtn"
+        ExecutarUsuarioEspecificoBtn.Size = New Size(111, 37)
+        ExecutarUsuarioEspecificoBtn.TabIndex = 54
+        ExecutarUsuarioEspecificoBtn.Text = "Executar"
+        ExecutarUsuarioEspecificoBtn.UseVisualStyleBackColor = True
+        ' 
         ' Usuario_Original
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
-        Controls.Add(SelecionarBancoLbl)
-        Controls.Add(SelecionarBancoBbx)
-        Controls.Add(ConectarBtn)
-        Controls.Add(SenhaTabelaTxb)
-        Controls.Add(NomeConectarTabelaTxb)
+        ClientSize = New Size(832, 303)
+        Controls.Add(ExecutarUsuarioEspecificoBtn)
+        Controls.Add(SelecionarUsuarioLbl)
+        Controls.Add(SelecionarUsuarioCbx)
+        Controls.Add(Button1)
+        Controls.Add(ExecutarTodosLbl)
+        Controls.Add(ExecutarTodosUsuariosBtn)
+        Controls.Add(SelecionarBancoUsuarioLbl)
+        Controls.Add(SelecionarBancoUsuarioBbx)
+        Controls.Add(ConectarUsuarioBtn)
+        Controls.Add(SenhaUsusarioTxb)
+        Controls.Add(NomeusuarioTxb)
         Controls.Add(ServidorUsuarioTxb)
-        Controls.Add(SenhaConectarLbl)
+        Controls.Add(SenhaUsuarioLbl)
         Controls.Add(UsuarioLbl)
         Controls.Add(ServidorUsuarioLbl)
         Name = "Usuario_Original"
@@ -144,13 +218,19 @@ Partial Class Usuario_Original
         PerformLayout()
     End Sub
 
-    Friend WithEvents SelecionarBancoLbl As Label
-    Friend WithEvents SelecionarBancoBbx As ComboBox
-    Friend WithEvents ConectarBtn As Button
-    Friend WithEvents SenhaTabelaTxb As TextBox
-    Friend WithEvents NomeConectarTabelaTxb As TextBox
+    Friend WithEvents SelecionarBancoUsuarioLbl As Label
+    Friend WithEvents SelecionarBancoUsuarioBbx As ComboBox
+    Friend WithEvents ConectarUsuarioBtn As Button
+    Friend WithEvents SenhaUsusarioTxb As TextBox
+    Friend WithEvents NomeusuarioTxb As TextBox
     Friend WithEvents ServidorUsuarioTxb As TextBox
-    Friend WithEvents SenhaConectarLbl As Label
+    Friend WithEvents SenhaUsuarioLbl As Label
     Friend WithEvents UsuarioLbl As Label
     Friend WithEvents ServidorUsuarioLbl As Label
+    Friend WithEvents ExecutarTodosUsuariosBtn As Button
+    Friend WithEvents ExecutarTodosLbl As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents SelecionarUsuarioCbx As ComboBox
+    Friend WithEvents SelecionarUsuarioLbl As Label
+    Friend WithEvents ExecutarUsuarioEspecificoBtn As Button
 End Class
--- SuporteHelp/Usuario Original.vb
+++ SuporteHelp/Usuario Original.vb
@@ -1,3 +1,56 @@
-﻿Public Class Usuario_Original
+﻿Imports System.Data.SqlClient
 
+Public Class Usuario_Original
+    Private Sub ConectarUsuarioBtn_Click(sender As Object, e As EventArgs) Handles ConectarUsuarioBtn.Click
+        ' Captura os valores digitados nos textboxes de servidor, usuário e senha
+        Dim servidor As String = ServidorUsuarioTxb.Text
+        Dim usuario As String = NomeusuarioTxb.Text
+        Dim senha As String = SenhaUsusarioTxb.Text
+
+        ' Desabilita o botão de pesquisa
+        ExecutarTodosUsuariosBtn.Enabled = False
+        SelecionarUsuarioCbx.Enabled = False
+        ExecutarUsuarioEspecificoBtn.Enabled = False
+        '' Verifica se todos os campos foram preenchidos
+        If String.IsNullOrEmpty(servidor) OrElse String.IsNullOrEmpty(usuario) OrElse String.IsNullOrEmpty(senha) Then
+            MessageBox.Show("Preencha todos os campos antes de conectar.")
+            Return
+        End If
+
+        ' Cria uma string de conexão com o servidor de banco de dados
+        Dim conexao As String = "Server=" & servidor & ";User Id=" & usuario & ";Password=" & senha
+
+        Try
+            ' Cria uma conexão com o servidor de banco de dados
+            Using conexaoBD As New SqlConnection(conexao)
+                ' Abre a conexão com o servidor
+                conexaoBD.Open()
+
+                '' Exibe uma mensagem de sucesso ao conectar no servidor
+                MessageBox.Show("Conexão estabelecida com sucesso, Selecione o Banco!")
+
+                ' Executa uma consulta SQL que retorna todos os bancos de dados do servidor
+                Dim comando As New SqlCommand("SELECT name as 'Nome', create_date as 'Data' FROM sys.databases WHERE name NOT IN ('master', 'tempdb', 'model', 'msdb')", conexaoBD)
+                Dim leitor As SqlDataReader = comando.ExecuteReader()
+
+                ' Cria uma lista para armazenar os nomes dos bancos de dados
+                Dim listaBancos As New List(Of String)
+
+                ' Popula a lista com os nomes dos bancos de dados
+                While leitor.Read()
+                    listaBancos.Add(leitor("Nome").ToString())
+                End While
+
+                ' Fecha o leitor de dados
+                leitor.Close()
+
+                ' Popula o combobox com os nomes dos bancos de dados
+                SelecionarBancoUsuarioBbx.DataSource = listaBancos
+
+            End Using
+        Catch ex As Exception
+            ' Exibe uma mensagem de erro caso ocorra uma exceção
+            MessageBox.Show("Erro ao carregar bancos de dados: " & ex.Message)
+        End Try
+    End Sub
 End Class
