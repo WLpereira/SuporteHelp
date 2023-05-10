ea7dceff70bbcf3c7d467cbd3d3960b804a3df4a
Administrator <washingtton_hotmail.com>
2023-05-10 10:44:56 -0300

    s

---
+++ SuporteHelp/SqlDataSourceEnumerator.vb
@@ -0,0 +1,8 @@
+﻿Friend Class SqlDataSourceEnumerator
+    Public Sub New()
+    End Sub
+
+    Friend Function GetDataSources() As DataTable
+        Throw New NotImplementedException()
+    End Function
+End Class
