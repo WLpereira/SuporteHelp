88e1d9d0e547cb1bc081cd43c36deb73989139d3
WLPereira <washingtton_hotmail.com>
2023-07-26 11:53:07 -0300

    ordenação

--- SuporteHelp/Descricao_Coluna.vb
+++ SuporteHelp/Descricao_Coluna.vb
@@ -29,7 +29,7 @@ Public Class Descricao_Coluna
                 MessageBox.Show("Conexão estabelecida com sucesso, Selecione o Banco!")
 
                 ' Executa uma consulta SQL que retorna todos os bancos de dados do servidor
-                Dim comando As New SqlCommand("SELECT name as 'Nome', create_date as 'Data' FROM sys.databases WHERE name NOT IN ('master', 'tempdb', 'model', 'msdb')", conexaoBD)
+                Dim comando As New SqlCommand("SELECT name as 'Nome', create_date as 'Data' FROM sys.databases WHERE name NOT IN ('master', 'tempdb', 'model', 'msdb')oreder by name", conexaoBD)
                 Dim leitor As SqlDataReader = comando.ExecuteReader()
 
                 ' Cria uma lista para armazenar os nomes dos bancos de dados
